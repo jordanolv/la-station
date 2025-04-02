271db0f6d20565721a4828ff327b1780eb67f482
Jordan <olivier.jordan21@gmail.com>
2025-04-02 15:55:31 +0200

    fix(ci/cd): url cwd

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,8 +2,8 @@ module.exports = {
   apps: [
     {
       name: "la-station-bot",
-      script: "dist/bot/app.js",
-      cwd: "./src/bot",
+      script: "./dist/bot/app.js",
+      cwd: ".",
       env: {
         NODE_ENV: "development",
       },
