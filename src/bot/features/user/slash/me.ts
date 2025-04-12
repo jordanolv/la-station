cbbdb56d63674fea2357d9dca3a86d4ec498289c
Jordan <olivier.jordan21@gmail.com>
2025-04-12 03:12:19 +0200

    update secrets

--- src/bot/features/user/slash/me.ts
+++ src/bot/features/user/slash/me.ts
@@ -1,7 +1,7 @@
 import { SlashCommandBuilder } from '@discordjs/builders';
 import { ChatInputCommandInteraction, EmbedBuilder } from 'discord.js';
-import { BotClient } from '../../../../BotClient';
-import UserModel from '../../../../../database/models/User';
+import { BotClient } from '../../../BotClient';
+import UserModel from '../../../../database/models/User';
 import { UserService } from '@database/services/UserService';
 
 function formatDate(date: Date): string { 
