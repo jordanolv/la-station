e2a553c673b2513c15bab7f6a036124c7c9a1d58
Jordan <olivier.jordan21@gmail.com>
2026-03-03 11:23:24 +0100

    30min to unlock mountain

--- src/features/voc-manager/constants/vocManager.constants.ts
+++ src/features/voc-manager/constants/vocManager.constants.ts
@@ -1,4 +1,4 @@
 export const VOC_CONFIG_BUTTON_ID = 'voc-config-button';
 export const VOC_INVITE_USER_SELECT_ID = 'voc-invite-user-select';
 
-export const MOUNTAIN_REQUIRED_SECONDS = 30; // 30 minutes
+export const MOUNTAIN_REQUIRED_SECONDS = 30 * 60; // 30 minutes
