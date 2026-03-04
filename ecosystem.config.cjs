b94215a0443406b7db7ec574982bf8e5745b93f7
Jordan <olivier.jordan21@gmail.com>
2026-03-04 14:22:48 +0100

    change pm2 name

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: "la-station-bot",
+      name: "the-ridge-bot",
       script: "dist/index.js",
       env_file: ".env",
       env: { NODE_ENV: "production" },
